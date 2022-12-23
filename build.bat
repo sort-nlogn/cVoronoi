21ff25bf972a04b497ca7e773708d7c6fd5dcee9
sort-nlogn <artomstepanov444@gmail.com>
2022-12-23 22:19:38 +0300

    Render

---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo off
+C:/TDM-GCC-32/bin/g++.exe -g .\main.cpp -o .\main.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 && main.exe
--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <graphics.h>
 
-#define max_polygon_size 100
-#define sites_cnt 30
+#define max_polygon_size 15
+#define sites_cnt 2500
 #define eps 0.00000001
 
+#define width 640
+#define height 480
 
 #define format_point "(%f, %f)"
 #define r() ((double)rand() / RAND_MAX)
 
 typedef struct{
     double a, b, c;
-}line;
+}Line;
 
 typedef struct{
     double x, y;
@@ -24,7 +27,7 @@ typedef struct{
     point2d points[max_polygon_size];
 }polygon;
 
-double get_signed_dist(line l, point2d p){
+double get_signed_dist(Line l, point2d p){
     return l.a * p.x + l.b * p.y + l.c;
 }
 
@@ -32,12 +35,12 @@ double dot2d(point2d p1, point2d p2){
     return p1.x*p2.x + p1.y*p2.y;
 }
 
-line get_bisection(point2d p1, point2d p2){
+Line get_bisection(point2d p1, point2d p2){
     double a = -(p2.x - p1.x), b = -(p2.y - p1.y);
     point2d mid_p = {(p1.x + p2.x) / 2.0, (p1.y + p2.y) / 2}; 
     double c = -(a*mid_p.x + b*mid_p.y);
 
-    line l = {a, b, c};
+    Line l = {a, b, c};
     double d = get_signed_dist(l, p1);
 
     if(d < -eps){
@@ -47,7 +50,7 @@ line get_bisection(point2d p1, point2d p2){
     return l;
 }
 
-point2d seg_to_line_intersection(point2d p1, point2d p2, line l){
+point2d seg_to_line_intersection(point2d p1, point2d p2, Line l){
     point2d rd = {p2.x - p1.x, p2.y - p1.y};
     point2d n = {l.a, l.b};
     double t = -(l.c + dot2d(p1, n)) / dot2d(rd, n);
@@ -57,7 +60,7 @@ point2d seg_to_line_intersection(point2d p1, point2d p2, line l){
     return res;
 }
 
-void cut_negative(polygon *polygons, int idx, line l){
+void cut_negative(polygon *polygons, int idx, Line l){
     int sz = polygons[idx].size, new_sz = 0;
     polygon buffer;
     for (int i = 0; i < sz; i++){
@@ -81,7 +84,14 @@ void cut_negative(polygon *polygons, int idx, line l){
     polygons[idx].size = new_sz;
 }
 
+point2d on_circle(int cnt, int idx, double offset_x, double offset_y){
+    double step = (2.0 * M_PI) / cnt;
+    point2d pt = {cos(step * idx) + offset_x, sin(step * idx) + offset_y};
+    return pt;
+}
+
 void construct_voronoi(polygon *cells, point2d sites[]){
+    double step = (2.0 * M_PI) / sites_cnt;
     for(int i = 0; i < sites_cnt; i++){
         cells[i].size = 4;
 
@@ -90,43 +100,57 @@ void construct_voronoi(polygon *cells, point2d sites[]){
         cells[i].points[2].x = 1.0;  cells[i].points[2].y = 1.0;
         cells[i].points[3].x = 0.0;  cells[i].points[3].y = 1.0;
 
-        sites[i].x = r(); sites[i].y = r();  
+        // sites[i] = on_circle(sites_cnt, i, 0.5, 0.5);
+        sites[i].x = r();
+        sites[i].y = r();
 
-    }
-    FILE *f = fopen("out.txt", "w");
-    for(int i = 0; i < sites_cnt; i++){
-        fprintf(f, format_point, sites[i].x, sites[i].y);
-        fprintf(f, "\n");
     }
 
     for(int i = 0; i < sites_cnt; i++){
         for(int j = 0; j < sites_cnt; j++){
             if(i == j) continue;
-            line bisec = get_bisection(sites[i], sites[j]);
+            Line bisec = get_bisection(sites[i], sites[j]);
             cut_negative(cells, i, bisec);
         }
     }
 
-    for(int i = 0; i < sites_cnt; i++){
-        fprintf(f, "\\operatorname{polygon} (");
-        for(int j = 0; j < cells[i].size; j++){
-            fprintf(f, format_point, cells[i].points[j].x, cells[i].points[j].y);
-            fprintf(f, ",");
-        }
-        fprintf(f, format_point, cells[i].points[cells[i].size - 1].x, cells[i].points[cells[i].size - 1].y);
-        fprintf(f, ")\n");
-    }
-
-    fclose(f);
-
 }
 
-int main(){
+
+
+int main(int argc, char *argv[]){
     srand(time(NULL));
     point2d sites[sites_cnt];
-    polygon *Voronoi_cells = malloc(sizeof(polygon) * sites_cnt);
+    polygon *Voronoi_cells = (polygon *)malloc(sizeof(polygon) * sites_cnt);
+    int colors[sites_cnt]; for (int i = 0; i < sites_cnt; i++){colors[i] = RGB((int)(r() * 255), (int)(r() * 255), (int)(r() * 255));}
 
-    construct_voronoi(Voronoi_cells, sites);    
+    construct_voronoi(Voronoi_cells, sites); 
+ 
+    int win = initwindow(width, height, "Voronoi");
+
+    while(1){
+        for(int i = 0; i < sites_cnt; i++){
+            int *shape = (int*)malloc((Voronoi_cells[i].size + 1) * 2 * sizeof(int));
+            for(int j = 0; j < Voronoi_cells[i].size; j++){
+                point2d p1 = Voronoi_cells[i].points[j];
+                shape[2 * j + 0] = (int)(p1.x * width);
+                shape[2 * j + 1] = (int)(p1.y * height);
+
+            }
+            shape[2 * Voronoi_cells[i].size + 0] = shape[0];
+            shape[2 * Voronoi_cells[i].size + 1] = shape[1];
+
+            int col = colors[i];
+            setcolor(col);
+            setfillstyle(SOLID_FILL, col);
+            fillpoly(Voronoi_cells[i].size + 1, shape);
+            free(shape);
+        }
+            swapbuffers();
+            clearviewport();
+            Sleep(1);
+        }
+
     free(Voronoi_cells);
     return 0;
 }
